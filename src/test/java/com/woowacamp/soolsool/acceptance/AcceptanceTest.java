fc070a1b5fae0d35510f24e577ec72cc2438ad5e
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-12-18 00:17:50 +0900

    refactor: 테스트 격리를 dirtyContext 지우고 DatabaseCleaner 방식으로 변경

--- src/test/java/com/woowacamp/soolsool/acceptance/AcceptanceTest.java
+++ src/test/java/com/woowacamp/soolsool/acceptance/AcceptanceTest.java
@@ -1,19 +1,16 @@
 package com.woowacamp.soolsool.acceptance;
 
+import com.woowacamp.soolsool.config.DatabaseCleaner;
 import com.woowacamp.soolsool.config.RedisTestConfig;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
-@Sql({"/liquor-type.sql", "/member-type.sql", "/order-type.sql", "/receipt-type.sql"})
 @Import(RedisTestConfig.class)
 public abstract class AcceptanceTest {
 
@@ -22,8 +19,13 @@ public abstract class AcceptanceTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        databaseCleaner.execute();
+        databaseCleaner.insert();
     }
 }
