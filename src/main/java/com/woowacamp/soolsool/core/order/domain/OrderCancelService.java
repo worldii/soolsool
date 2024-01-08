44e52984f368a62d4672eea238629267085f052f
Jongha Park <jongha2788@u.sogang.ac.kr>
2024-01-08 17:25:28 +0900

    refactor : 주문 취소 되었을 때, 멤버 마일리지를 다시 되돌린다.

--- src/main/java/com/woowacamp/soolsool/core/order/domain/OrderCancelService.java
+++ src/main/java/com/woowacamp/soolsool/core/order/domain/OrderCancelService.java
@@ -1,19 +1,41 @@
 package com.woowacamp.soolsool.core.order.domain;
 
-import static com.woowacamp.soolsool.core.order.domain.vo.OrderStatusType.CANCELED;
-
+import com.woowacamp.soolsool.core.member.application.MemberService;
+import com.woowacamp.soolsool.core.member.dto.request.MemberMileageChargeRequest;
+import com.woowacamp.soolsool.core.order.exception.OrderErrorCode;
+import com.woowacamp.soolsool.global.exception.SoolSoolException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+import static com.woowacamp.soolsool.core.order.domain.vo.OrderStatusType.CANCELED;
+import static com.woowacamp.soolsool.core.order.exception.OrderErrorCode.ACCESS_DENIED_ORDER;
 
 @Service
 @RequiredArgsConstructor
 public class OrderCancelService {
 
     private final OrderStatusService orderStatusService;
-    
+    private final MemberService memberService;
+    private final OrderRepository orderRepository;
+
     @Transactional
     public Long cancelOrder(final Long memberId, final Long orderId) {
-        return orderStatusService.modifyOrderStatusType(memberId, orderId, CANCELED);
+        final Order order = orderRepository
+                .findOrderById(orderId).orElseThrow(() -> new SoolSoolException(OrderErrorCode.NOT_EXISTS_ORDER));
+
+        validateAccessible(memberId, order);
+
+        memberService.addMemberMileage(memberId, new MemberMileageChargeRequest(order.getMileageUsage()));
+
+        return orderStatusService.modifyOrderStatusType(memberId, order.getId(), CANCELED);
+    }
+
+    private void validateAccessible(final Long memberId, final Order order) {
+        if (!Objects.equals(memberId, order.getMemberId())) {
+            throw new SoolSoolException(ACCESS_DENIED_ORDER);
+        }
     }
 }
--- src/test/java/com/woowacamp/soolsool/core/order/application/OrderServiceIntegrationTest.java
+++ src/test/java/com/woowacamp/soolsool/core/order/application/OrderServiceIntegrationTest.java
@@ -1,14 +1,9 @@
 package com.woowacamp.soolsool.core.order.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
+import com.woowacamp.soolsool.core.member.application.MemberService;
+import com.woowacamp.soolsool.core.member.domain.MemberRoleCache;
 import com.woowacamp.soolsool.core.member.domain.OrderMemberServiceImpl;
-import com.woowacamp.soolsool.core.order.domain.Order;
-import com.woowacamp.soolsool.core.order.domain.OrderCancelService;
-import com.woowacamp.soolsool.core.order.domain.OrderQueryRepository;
-import com.woowacamp.soolsool.core.order.domain.OrderRepository;
-import com.woowacamp.soolsool.core.order.domain.OrderStatusCache;
+import com.woowacamp.soolsool.core.order.domain.*;
 import com.woowacamp.soolsool.core.order.domain.vo.OrderStatusType;
 import com.woowacamp.soolsool.global.config.CacheManagerConfig;
 import com.woowacamp.soolsool.global.config.QuerydslConfig;
@@ -21,13 +16,17 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @DataJpaTest
 @Import(
-    {
-        OrderService.class, OrderStatusCache.class, OrderMemberServiceImpl.class,
-        OrderQueryRepository.class, QuerydslConfig.class, RedissonConfig.class,
-        CacheManagerConfig.class, OrderCancelService.class
-    }
+        {
+                OrderService.class, OrderStatusCache.class, OrderMemberServiceImpl.class,
+                OrderQueryRepository.class, QuerydslConfig.class, RedissonConfig.class,
+                CacheManagerConfig.class, OrderCancelService.class, OrderStatusService.class,
+                MemberService.class, MemberRoleCache.class
+        }
 )
 @DisplayName("통합 테스트: OrderService")
 class OrderServiceIntegrationTest {
@@ -39,11 +38,11 @@ class OrderServiceIntegrationTest {
 
     @Test
     @Sql({
-        "/member-type.sql", "/member.sql",
-        "/liquor-type.sql", "/liquor.sql",
-        "/cart-item.sql",
-        "/receipt-type.sql", "/receipt.sql",
-        "/order-type.sql", "/order.sql"
+            "/member-type.sql", "/member.sql",
+            "/liquor-type.sql", "/liquor.sql",
+            "/cart-item.sql",
+            "/receipt-type.sql", "/receipt.sql",
+            "/order-type.sql", "/order.sql"
     })
     @DisplayName("주문 상세 내역 조회 시 주문이 존재하지 않을 경우 SoolSoolException을 던진다.")
     void failOrderDetailWhenNotExistsOrder() {
@@ -52,17 +51,17 @@ class OrderServiceIntegrationTest {
 
         // when & then
         assertThatThrownBy(() -> orderService.orderDetail(김배달, 99999L))
-            .isExactlyInstanceOf(SoolSoolException.class)
-            .hasMessage("주문 내역이 존재하지 않습니다.");
+                .isExactlyInstanceOf(SoolSoolException.class)
+                .hasMessage("주문 내역이 존재하지 않습니다.");
     }
 
     @Test
     @Sql({
-        "/member-type.sql", "/member.sql",
-        "/liquor-type.sql", "/liquor.sql",
-        "/cart-item.sql",
-        "/receipt-type.sql", "/receipt.sql",
-        "/order-type.sql", "/order.sql"
+            "/member-type.sql", "/member.sql",
+            "/liquor-type.sql", "/liquor.sql",
+            "/cart-item.sql",
+            "/receipt-type.sql", "/receipt.sql",
+            "/order-type.sql", "/order.sql"
     })
     @DisplayName("다른 사용자의 주문 상세내역을 조회할 경우 SoolSoolException을 던진다.")
     void failOrderDetailWhenAccessToOthers() {
@@ -72,17 +71,17 @@ class OrderServiceIntegrationTest {
 
         // when & then
         assertThatThrownBy(() -> orderService.orderDetail(최민족, 김배달_주문))
-            .isExactlyInstanceOf(SoolSoolException.class)
-            .hasMessage("본인의 주문 내역만 조회할 수 있습니다.");
+                .isExactlyInstanceOf(SoolSoolException.class)
+                .hasMessage("본인의 주문 내역만 조회할 수 있습니다.");
     }
 
     @Test
     @Sql({
-        "/member-type.sql", "/member.sql",
-        "/liquor-type.sql", "/liquor.sql",
-        "/cart-item.sql",
-        "/receipt-type.sql", "/receipt.sql",
-        "/order-type.sql", "/order.sql"
+            "/member-type.sql", "/member.sql",
+            "/liquor-type.sql", "/liquor.sql",
+            "/cart-item.sql",
+            "/receipt-type.sql", "/receipt.sql",
+            "/order-type.sql", "/order.sql"
     })
     @DisplayName("주문 상태 변경 시 Order가 존재하지 않을 경우 SoolSoolException을 던진다.")
     void failModifyOrderWhenNotExistsOrder() {
@@ -91,17 +90,17 @@ class OrderServiceIntegrationTest {
 
         // when & then
         assertThatThrownBy(() -> orderService.cancelOrder(김배달, 99999L))
-            .isExactlyInstanceOf(SoolSoolException.class)
-            .hasMessage("주문 내역이 존재하지 않습니다.");
+                .isExactlyInstanceOf(SoolSoolException.class)
+                .hasMessage("주문 내역이 존재하지 않습니다.");
     }
 
     @Test
     @Sql({
-        "/member-type.sql", "/member.sql",
-        "/liquor-type.sql", "/liquor.sql",
-        "/cart-item.sql",
-        "/receipt-type.sql", "/receipt.sql",
-        "/order-type.sql", "/order.sql"
+            "/member-type.sql", "/member.sql",
+            "/liquor-type.sql", "/liquor.sql",
+            "/cart-item.sql",
+            "/receipt-type.sql", "/receipt.sql",
+            "/order-type.sql", "/order.sql"
     })
     @DisplayName("다른 사용자의 주문 상세내역을 변경할 경우 SoolSoolException을 던진다.")
     void failModifyOrderWhenAccessToOthers() {
@@ -111,17 +110,17 @@ class OrderServiceIntegrationTest {
 
         // when & then
         assertThatThrownBy(() -> orderService.cancelOrder(최민족, 김배달_주문))
-            .isExactlyInstanceOf(SoolSoolException.class)
-            .hasMessage("본인의 주문 내역만 조회할 수 있습니다.");
+                .isExactlyInstanceOf(SoolSoolException.class)
+                .hasMessage("본인의 주문 내역만 조회할 수 있습니다.");
     }
 
     @Test
     @Sql({
-        "/member-type.sql", "/member.sql",
-        "/liquor-type.sql", "/liquor.sql",
-        "/cart-item.sql",
-        "/receipt-type.sql", "/receipt.sql",
-        "/order-type.sql", "/order.sql"
+            "/member-type.sql", "/member.sql",
+            "/liquor-type.sql", "/liquor.sql",
+            "/cart-item.sql",
+            "/receipt-type.sql", "/receipt.sql",
+            "/order-type.sql", "/order.sql"
     })
     @DisplayName("주문을 취소한다.")
     void cancelOrder() throws Exception {
@@ -134,7 +133,7 @@ class OrderServiceIntegrationTest {
 
         /* then */
         final Order order = orderRepository.findOrderById(orderId)
-            .orElseThrow(RuntimeException::new);
+                .orElseThrow(RuntimeException::new);
         assertThat(order.getStatus().getType()).isEqualTo(OrderStatusType.CANCELED);
     }
 }
